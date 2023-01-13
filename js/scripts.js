f31b24ff5df340b9d5f69e42255abe30cc171003
John <rohnfiggins@gmail.com>
2023-01-12 23:10:52 -0800

    working...

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,35 @@
 
 
 
-//ui logic
-function handleSelection(event){
-  event.preventDefault();
-  console.log(getElementById("selection"));
+//ui logic any code that accesses or interacts with the DOM
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log("it gets here");
+    //function should get selection
+    let prob = (document.querySelector("selection")).value;
+ console.log(prob);
+
+   
+  });
 
 }
 
 
-window.addEventListener("load", function(){
-  
-  console.log('hello');
-  item = document.getElementById("selection").value;
-  event.preventDefault();
-  console.log(item);
 
-})
+// function handleSelection(event){
+//   event.preventDefault();
+//   console.log(getElementById("selection"));
+
+// }
+
+
+// window.addEventListener("load", function(){
+  
+//   console.log('hello');
+//   item = document.getElementById("selection").value;
+//   event.preventDefault();
+//   console.log(item);
+
+// })
